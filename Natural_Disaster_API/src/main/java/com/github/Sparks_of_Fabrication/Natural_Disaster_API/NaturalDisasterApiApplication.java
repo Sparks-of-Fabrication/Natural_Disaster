7bf0cc7529dc22ffca9a0f397cdd6e7cfa78e21a
PetkoPetkov03 <komandir1612@gmail.com>
2025-02-19 21:05:09 +0200

    adding commands, setting up env

--- Natural_Disaster_API/src/main/java/com/github/Sparks_of_Fabrication/Natural_Disaster_API/NaturalDisasterApiApplication.java
+++ Natural_Disaster_API/src/main/java/com/github/Sparks_of_Fabrication/Natural_Disaster_API/NaturalDisasterApiApplication.java
@@ -1,12 +1,19 @@
 package com.github.Sparks_of_Fabrication.Natural_Disaster_API;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Objects;
 
 @SpringBootApplication
 public class NaturalDisasterApiApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("DB_URL", Objects.requireNonNull(dotenv.get("DB_URL")));
+		System.setProperty("DB_USER", Objects.requireNonNull(dotenv.get("DB_USER")));
+		System.setProperty("DB_SECRET", Objects.requireNonNull(dotenv.get("DB_SECRET")));
 		SpringApplication.run(NaturalDisasterApiApplication.class, args);
 	}
 
